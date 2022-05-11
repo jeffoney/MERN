067af5d57d13cbaaf07aa9a902a4bba49a709183
Jeff Oney <jeff.oney@icloud.com>
2022-05-11 12:28:33 -0700

    jsx

--- axios-pokemon-api/src/components/Pokemon.jsx
+++ axios-pokemon-api/src/components/Pokemon.jsx
@@ -1,12 +1,30 @@
-import './App.css';
-import Pokemon from './components/Pokemon'
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <Pokemon />
+const Pokemon = (props) => {
+const [pokemonList, setPokemonList] = useState([]);
+
+const getPokemon = (e) => {
+axios.get("https://pokeapi.co/api/v2/pokemon?limit=807").then((response) => {
+    setPokemonList(response.data.results);
+})
+.catch(error => console.log(error))
+};
+
+return (
+    <div className="container-fluid text-center mt-5">
+    <button className="btn btn-lg btn-success" onClick={getPokemon}>
+        Fetch Pokemon
+    </button>
+    <ul className="mt-5 list-unstyled">
+        {
+        pokemonList.map( (pokemon, index) => {
+            return <li key={index}>{pokemon.name}</li>;
+        })
+        }
+    </ul>
     </div>
-  );
-}
+    );
+};
 
-export default App;
+export default Pokemon;
