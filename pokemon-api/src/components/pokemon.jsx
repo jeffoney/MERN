9b6f4d6023c9122fd88c6558404399544d204abd
Jeff Oney <jeff.oney@icloud.com>
2022-05-11 12:07:49 -0700

    Pokemon.jsx

--- pokemon-api/src/components/pokemon.jsx
+++ pokemon-api/src/components/pokemon.jsx
@@ -2,35 +2,35 @@ import React, {useState} from "react";
 
 const Pokemon = (props) => {
 
-  const [pokemonList, setPokemonList] = useState([]);
+const [pokemonList, setPokemonList] = useState([]);
 
-  const getPokemon = () => {
+const getPokemon = () => {
 
     fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
-      .then((response) => {
+    .then((response) => {
         return response.json();
-      })
-      .then((response) => {
+    })
+    .then((response) => {
         setPokemonList(response.results);
-      })
-      .catch((err) => {
+    })
+    .catch((err) => {
         console.log(err);
-      });
-  }
+    });
+}
 
-  return (
+return (
     <div className="container-fluid text-center mt-5">
-      <button className="btn btn-lg btn-success" onClick={getPokemon}>
+    <button className="btn btn-lg btn-success" onClick={getPokemon}>
         Fetch Pokemon
-      </button>
-      <ul className="mt-5 list-unstyled">
+    </button>
+    <ul className="mt-5 list-unstyled">
         {pokemonList.length > 0 &&
-          pokemonList.map((pokemon, index) => {
+        pokemonList.map((pokemon, index) => {
             return <li key={index}>{pokemon.name}</li>;
-          })}
-      </ul>
+        })}
+    </ul>
     </div>
-  );
+);
 }
 
 export default Pokemon;
