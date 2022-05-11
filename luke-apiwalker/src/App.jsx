307a961ad4eed06f59be555a0b747b705740758c
Jeff Oney <jeff.oney@icloud.com>
2022-05-11 12:56:16 -0700

    app.jsx update

--- luke-apiwalker/src/App.jsx
+++ luke-apiwalker/src/App.jsx
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
+import Search from './components/Search'
+import Result from './components/Result'
+import {Router} from '@reach/router'
 
 function App() {
+
+    const [searchInfo, setSearchInfo] = useState({
+      category: "people",
+      id: "",
+    });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Search searchInfo={searchInfo} setSearchInfo={setSearchInfo} />
+
+      <Router>
+        <Result path="/:category/:id" searchInfo={searchInfo}/>
+      </Router>
     </div>
   );
 }
