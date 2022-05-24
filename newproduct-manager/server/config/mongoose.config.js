4575177030067fcd89aefec69600bd35f0de1543
Jeff Oney <jeff.oney@icloud.com>
2022-05-24 11:59:14 -0700

    config

--- newproduct-manager/server/config/mongoose.config.js
+++ newproduct-manager/server/config/mongoose.config.js
@@ -2,12 +2,12 @@
 // DATABASE SETUP
 // ---------------------------------------------------
 
-// 1) Importing External Libraries (Moongose)
+// 1) Importing External Libraries (Mongoose)
 const mongoose = require("mongoose"); 
 
 // 2) Setting connection to Mongo DB using 'mongoose' instance
 mongoose.connect("mongodb://localhost/product-manager-api",{
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => console.log("Sucessfully connected to Database"))
+}).then(() => console.log("Successfully connected to Database"))
   .catch(err => console.log("Something went wrong when connecting to the database: ", err));
