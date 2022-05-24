413756ff601ea83d698d4ce7a559d1c04ec66c60
Jeff Oney <jeff.oney@icloud.com>
2022-05-24 12:02:08 -0700

    product.models

---
+++ newproduct-manager/server/models/product.model.js
@@ -0,0 +1,19 @@
+// ---------------------------------------------------
+// MODEL SETUP
+// ---------------------------------------------------
+
+// 1) Importing External Libraries (Mongoose)
+const mongoose = require('mongoose'); 
+
+// 2) Creating Schema for Model (blueprint)
+const ProductSchema = new mongoose.Schema({
+    title: { type: String },
+    price: { type: Number },
+    description: { type: String }  
+}, { timestamps: true });
+
+// 3) Creating collection by Schema
+const ProductModel = mongoose.model("Product", ProductSchema);
+
+// 4) Exporting Model
+module.exports = ProductModel;
