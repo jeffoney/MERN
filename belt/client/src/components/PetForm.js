30e6950c13f08f8e2573142a8fefbee6b8e26fb8
Jeff Oney <jeff.oney@icloud.com>
2022-05-25 12:47:55 -0700

    route bug

--- belt/client/src/components/PetForm.js
+++ belt/client/src/components/PetForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { navigate } from '@reach/router';
+// import { navigate } from '@reach/router';
 
 export default () => {
     const [name, setName] = useState("");
--- belt/client/src/components/PetInfo.js
+++ belt/client/src/components/PetInfo.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { navigate } from '@reach/router';
+// import { navigate } from '@reach/router';
 
 
 export default props => {
